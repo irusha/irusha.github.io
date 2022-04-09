ed7debf146104bd5de96da2c3bd901373e413085
Irusha Sansuka <78542929+irusha@users.noreply.github.com>
2022-04-09 14:43:56 +0530

    test web

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var counterContainer = document.querySelector(".website-counter");
-var resetButton = document.querySelector("#reset");
 var visitCount = localStorage.getItem("page_view");
 
 // Check if page_view entry is present
