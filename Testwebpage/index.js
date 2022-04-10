5c79ca915fa842300e56584389a9191f1e95287a
Irusha Sansuka <78542929+irusha@users.noreply.github.com>
2022-04-10 11:44:05 +0530

    added menu

--- Testwebpage/index.js
+++ Testwebpage/index.js
@@ -1,10 +1,24 @@
+var c = true;
+var menubutton = document.getElementById("menubicon")
 document.querySelector(".button_cart").addEventListener("click", function(){
     this.closest(".banner").style.display = "none";
 });
 function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
+  if (c){
+    document.getElementById("rightMenu").style.display = "block";
+    console.log(menubutton.textContent);
+    menubutton.textContent = 'close'
+    c = false;
   }
+  else{
+    document.getElementById("rightMenu").style.display = "none";
+    c = true;
+    console.log(menubutton.textContent);
+    menubutton.textContent = 'menu'
+  }
+    
+}
   
   function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("leftMenu").style.display = "none";
   }
